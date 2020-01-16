d4717299b4df52ec4a004155f43336751cf24953
Takeshi Tsukamoto <itometeam@gmail.com>
2020-01-16 19:26:01 +0900

    Chagen font

--- init.el
+++ init.el
@@ -272,7 +272,7 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-colorize-cursor-according-to-state t
 
    ;; Default font or prioritized list of fonts.
-   dotspacemacs-default-font '("Source Code Pro"
+   dotspacemacs-default-font '("Jetbrains Mono"
                                :weight normal
                                :width normal)
 
